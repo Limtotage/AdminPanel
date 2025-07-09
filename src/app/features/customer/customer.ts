169b955fb07afcd72c107710b6591bd244388f07
Limtotage <abdullahltgyalim@gmail.com>
2025-07-09 18:22:31 +0300

    Customer Panel Done. Day13

--- src/app/features/customer/customer.ts
+++ src/app/features/customer/customer.ts
@@ -5,24 +5,38 @@ import { HttpClient } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-customer',
   standalone: true,
-  imports: [CommonModule, RouterModule, MatCheckboxModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
   templateUrl: './customer.html',
 })
 export class CustomerComponent implements OnInit {
   products: any[] = [];
+  myProducts: any[] = [];
   currentUser: any;
   selectedProductIds: Set<number> = new Set();
+  UnselectProductIds: Set<number> = new Set();
 
   constructor(private authservice: AuthService, private http: HttpClient) {}
+  refreshProducts() {
+    this.loadProducts();
+    this.loadOwnProducts();
+  }
 
   ngOnInit(): void {
     this.authservice.getCurrentUser().subscribe((user) => {
       this.currentUser = user;
       console.log('Giriş yapan kullanıcı:', user);
+      this.loadOwnProducts();
     });
     this.loadProducts();
   }
@@ -31,24 +45,50 @@ export class CustomerComponent implements OnInit {
       .get<any[]>('http://localhost:8080/api/product')
       .subscribe((data) => (this.products = data));
   }
+
+  loadOwnProducts() {
+    console.log(this.currentUser.id);
+    this.http
+      .get<any[]>(
+        `http://localhost:8080/api/product/customer/${this.currentUser.id}`
+      )
+      .subscribe((data) => (this.myProducts = data));
+  }
   toggleProduct(id: number, checked: boolean) {
     if (checked) this.selectedProductIds.add(id);
     else this.selectedProductIds.delete(id);
   }
+  untoggleProduct(id: number, checked: boolean) {
+    if (checked) this.UnselectProductIds.add(id);
+    else this.UnselectProductIds.delete(id);
+  }
   submitSelectedProducts() {
     const selectedIds = Array.from(this.selectedProductIds);
-    const updatedCustomer = {
-      fullname: this.currentUser.fullname,
-      username: this.currentUser.username,
-      products: selectedIds,
-    };
     console.log(selectedIds);
     this.http
       .put(
-        `http://localhost:8080/api/customer/${this.currentUser.id}`,
-        updatedCustomer
+        `http://localhost:8080/api/customer/addProduct/${this.currentUser.id}`,
+        selectedIds
       )
-      .subscribe(() => alert('Seçili Ürünler Eklenmiştir.'));
+      .subscribe(() => {
+        alert('Seçili Ürünler Eklenmiştir.');
+        this.refreshProducts();
+        this.selectedProductIds = new Set();
+      });
+  }
+  removeProductFromList() {
+    const selectedIds = Array.from(this.UnselectProductIds);
+    console.log('Removed Ids: ' + selectedIds);
+    this.http
+      .put(
+        `http://localhost:8080/api/customer/removeProduct/${this.currentUser.id}`,
+        selectedIds
+      )
+      .subscribe(() => {
+        alert('Seçili Ürünler Kaldırılmıştır.');
+        this.refreshProducts();
+        this.UnselectProductIds = new Set();
+      });
   }
   deleteUser() {
     if (confirm('Hesabınızı silmek istediğinize emin misiniz?')) {
